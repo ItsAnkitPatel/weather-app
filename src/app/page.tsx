0479c7927728b636a220ce06f07ef9b8dacd3075
ItsAnkitPatel <itsakppatel@gmail.com>
2024-06-19 10:00:44 +0530

    add heading section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import SearchBar from "@/components/SearchBar";
+import LottiePlayer from "@/components/LottiePlayer";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-    </main>
+    <MaxWidthWrapper>
+      <div className="flex items-center max-sm:mb-2">
+        <LottiePlayer srcPath="/sun-animation.lottie" width={220} />
+        <h1 className="text-3xl lg:text-nowrap">Weather Wonders</h1>
+      </div>
+      <SearchBar />
+    </MaxWidthWrapper>
   );
 }

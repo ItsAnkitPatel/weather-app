20c0f5eae583de990b6023beb6a7d4c950c65e1a
ItsAnkitPatel <itsakppatel@gmail.com>
2024-06-19 23:05:25 +0530

    add toastify & update width for lottieplayer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,18 @@ import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import SearchBar from "@/components/SearchBar";
 import LottiePlayer from "@/components/LottiePlayer";
 import { AppContextProvider } from "@/context";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Home() {
   return (
     <AppContextProvider>
       <MaxWidthWrapper>
+        <ToastContainer />
         <div className="flex items-center max-sm:mb-2">
-          <LottiePlayer srcPath="/sun-animation.lottie" width={220} />
+          <div className="w-56">
+            <LottiePlayer srcPath="/sun-animation.lottie" />
+          </div>
           <h1 className="text-3xl lg:text-nowrap">Weather Wonders</h1>
         </div>
         <SearchBar />

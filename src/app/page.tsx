c1e9d38ea4347e55a35f6f7a23163c4e8ef699f2
ItsAnkitPatel <itsakppatel@gmail.com>
2024-06-19 19:11:21 +0530

    add context provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
+"use client";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import SearchBar from "@/components/SearchBar";
 import LottiePlayer from "@/components/LottiePlayer";
+import { AppContextProvider } from "@/context";
 
 export default function Home() {
   return (
-    <MaxWidthWrapper>
-      <div className="flex items-center max-sm:mb-2">
-        <LottiePlayer srcPath="/sun-animation.lottie" width={220} />
-        <h1 className="text-3xl lg:text-nowrap">Weather Wonders</h1>
-      </div>
-      <SearchBar />
-    </MaxWidthWrapper>
+    <AppContextProvider>
+      <MaxWidthWrapper>
+        <div className="flex items-center max-sm:mb-2">
+          <LottiePlayer srcPath="/sun-animation.lottie" width={220} />
+          <h1 className="text-3xl lg:text-nowrap">Weather Wonders</h1>
+        </div>
+        <SearchBar />
+      </MaxWidthWrapper>
+    </AppContextProvider>
   );
 }

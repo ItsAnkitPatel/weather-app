2a6b2e134b44992098f847c350eaedf413bbe3c9
ItsAnkitPatel <itsakppatel@gmail.com>
2024-06-20 00:53:01 +0530

    flashing loader for searchbar

---
+++ src/components/SearchLoader.tsx
@@ -0,0 +1,15 @@
+const SearchLoader = () => {
+  return (
+    <>
+      <span className="animate-flashing inline-block rounded-full">.</span>
+      <span className="animate-flashing inline-block rounded-full delay-200">
+        .
+      </span>
+      <span className="animate-flashing inline-block rounded-full delay-300">
+        .
+      </span>
+    </>
+  );
+};
+
+export default SearchLoader;

3e5c84179ac23db749b883debc4ee5dad3a904cc
ItsAnkitPatel <itsakppatel@gmail.com>
2024-06-22 02:12:14 +0530

    add zustand store setup

---
+++ src/store/weather.ts
@@ -0,0 +1,32 @@
+import { create } from "zustand";
+
+export type selectedCityType = {
+  name: string;
+  country: string;
+};
+interface weatherState {
+  enableCurrentWeather: boolean;
+  setEnableCurrentWeather: (enableCurrentWeather: boolean) => void;
+  weatherLoaderScreen: boolean;
+  setWeatherLoaderScreen: (searchStarted: boolean) => void;
+  weatherComponentVisibility: boolean;
+  setWeatherComponentVisibility: (viabilityOfWeatherComponent: boolean) => void;
+  selectedCity: selectedCityType;
+  setSelectedCity: (city: selectedCityType) => void;
+}
+
+export const useWeatherStore = create<weatherState>()((set) => ({
+  enableCurrentWeather: false,
+  setEnableCurrentWeather: (enableCurrentWeather: boolean) =>
+    set({ enableCurrentWeather }),
+  weatherLoaderScreen: false,
+  setWeatherLoaderScreen: (searchStarted: boolean) =>
+    set({ weatherLoaderScreen: searchStarted }),
+
+  weatherComponentVisibility: false,
+  setWeatherComponentVisibility: (viabilityOfWeatherComponent: boolean) =>
+    set({ weatherComponentVisibility: viabilityOfWeatherComponent }),
+  
+  selectedCity: { name: "", country: "" },
+  setSelectedCity: (city: selectedCityType) => set({ selectedCity: city }),
+}));
